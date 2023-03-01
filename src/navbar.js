d43f8e536cfbf496ede350ca0689a3ce2ede3673
Cooper Tingey <coopertingey@Coopers-MBP.lan>
2023-02-28 18:17:21 -1000

    added media queries for mobile view

--- src/navbar.js
+++ src/navbar.js
@@ -4,11 +4,11 @@ export default function Navbar() {
     return (
         <div className="container-fluid navbar-container">
             <div className="container navbar-wrapper">
-                <div className="row">
-                    <div className="col-6 left-side text-start">
+                <div className="row links">
+                    <div className="col-md-6 left-side text-start">
                         <Link to="/">North Shore Cleaning</Link>
                     </div>
-                    <div className="col-6 right-side text-end">
+                    <div className="col-md-6 right-side text-end">
                         <a href="#footer">Contact</a>
                         <Link to="/about">About Our Services</Link>
                     </div>
